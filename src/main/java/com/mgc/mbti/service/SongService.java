b1de00a063cf55610d71339c26a7fd25bc1014e3
YongWoo <aba1430@naver.com>
2021-12-23 16:00:34 +0900

    [ADD] SongService

---
+++ src/main/java/com/mgc/mbti/service/SongService.java
@@ -0,0 +1,10 @@
+package com.mgc.mbti.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SongService {
+
+}
