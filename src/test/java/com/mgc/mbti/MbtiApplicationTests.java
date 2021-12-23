20c4957314ea794843e8d2b8b965b3a79b74925e
YongWoo <aba1430@naver.com>
2021-12-23 15:57:17 +0900

    [ADD] Spring

---
+++ src/test/java/com/mgc/mbti/MbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgc.mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
