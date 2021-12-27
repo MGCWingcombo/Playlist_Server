f131dec36fb6eea03bc64fd9cf5c50592e16115e
YongWoo <aba1430@naver.com>
2021-12-27 16:59:20 +0900

    [ADD] MusicRepo

---
+++ src/main/java/com/mgc/mbti/repository/MusicRepository.java
@@ -0,0 +1,13 @@
+package com.mgc.mbti.repository;
+
+import com.mgc.mbti.domain.Music;
+import com.mgc.mbti.domain.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MusicRepository extends JpaRepository<Music, Long> {
+    List<Music> findAllByPlayList (Playlist playlist);
+}
