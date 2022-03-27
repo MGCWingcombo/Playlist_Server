83cfc04ef638eda863fc916e5fec41eabff561fa
YongWoo <aba1430@naver.com>
2022-03-27 20:46:49 +0900

    [ADD] Paging용 findAll 메소드 생성

--- src/main/java/com/mgc/mbti/repository/MusicRepository.java
+++ src/main/java/com/mgc/mbti/repository/MusicRepository.java
@@ -2,6 +2,8 @@ package com.mgc.mbti.repository;
 
 import com.mgc.mbti.domain.Music;
 import com.mgc.mbti.domain.Playlist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,6 @@ import java.util.Map;
 
 public interface MusicRepository extends JpaRepository<Music, Long> {
     List<Music> findAllByPlayList (Playlist playlist);
+
+    Page<Music> findAllByPlayList (Playlist playlist, Pageable pageable);
 }
