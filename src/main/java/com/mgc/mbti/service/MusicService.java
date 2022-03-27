762c7fc82e2cdac94178838526756eefac415079
YongWoo <aba1430@naver.com>
2022-03-27 20:47:24 +0900

    [ADD] Paging을 이용하여 DB에서 24곡만 뽑게 설정

--- src/main/java/com/mgc/mbti/service/MusicService.java
+++ src/main/java/com/mgc/mbti/service/MusicService.java
@@ -5,6 +5,8 @@ import com.mgc.mbti.domain.Playlist;
 import com.mgc.mbti.repository.MusicRepository;
 import com.mgc.mbti.repository.PlaylistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -18,11 +20,9 @@ public class MusicService {
     private final MusicRepository musicRepository;
 
     public List<Map<String,String>> getMusicList(String tag){
-        List<Music> musics= musicRepository.findAllByPlayList(playlistRepository.findByTag(tag));
-        Random random = new Random();
+        Page<Music> musicPage = musicRepository.findAllByPlayList(playlistRepository.findByTag(tag), PageRequest.of(0,24));
 
-
-        List<Map<String, String>> musicList = musics.stream().map(
+        List<Map<String, String>> musicList = musicPage.stream().map(
                 music -> {
                     Map<String, String> map = Map.of("Music",music.getMusic(),"Artist",music.getArtist(),"url",music.getUrl(),"urlImg",music.getUrlImg());
                     return map;
@@ -30,15 +30,6 @@ public class MusicService {
         ).collect(Collectors.toList());
 
         Collections.shuffle(musicList);
-        /*
-        while (true){
-            if (musicList.size() == 8)
-                break;
-            else {
-                int i = random.nextInt(musicList.size());
-                musicList.remove(i);
-            }
-        }*/ // 8곡만 도려내는 기능이지만 최적화 이슈와 기능상 8곡만 보낼 이유가 없기에 삭제
 
         return musicList;
     }
