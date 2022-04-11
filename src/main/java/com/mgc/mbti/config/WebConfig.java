371e960d05e8df7d8b196e02c9360af9e94c0f3d
YongWoo <aba1430@naver.com>
2022-04-12 00:11:18 +0900

    [ADD] Domain 설정 이후 Cors 설정

--- src/main/java/com/mgc/mbti/config/WebConfig.java
+++ src/main/java/com/mgc/mbti/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("'http://52.78.85.15:8080")
+                .allowedMethods("GET");
     }
 }
