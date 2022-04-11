510c3e3a4177c3c7813e8f010e68bfc85e1721b9
YongWoo <aba1430@naver.com>
2022-04-11 17:27:54 +0900

    [FIX] CustomRepo를 사용하면서 쓰지않게된 로직 삭제

--- src/main/java/com/mgc/mbti/repository/MusicRepository.java
+++ src/main/java/com/mgc/mbti/repository/MusicRepository.java
@@ -2,6 +2,7 @@ package com.mgc.mbti.repository;
 
 import com.mgc.mbti.domain.Music;
 import com.mgc.mbti.domain.Playlist;
+import com.mgc.mbti.repository.customRepo.MusicCustomRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +11,5 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Map;
 
-public interface MusicRepository extends JpaRepository<Music, Long> {
-    List<Music> findAllByPlayList (Playlist playlist);
-
-    Page<Music> findAllByPlayList (Playlist playlist, Pageable pageable);
+public interface MusicRepository extends JpaRepository<Music, Long> , MusicCustomRepository {
 }
