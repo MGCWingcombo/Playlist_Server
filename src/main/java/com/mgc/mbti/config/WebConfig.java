72baf83023636fe2071bbf569296b00edf77ab69
YongWoo <aba1430@naver.com>
2022-04-12 00:35:14 +0900

    [FIX] 오타 수정..

--- src/main/java/com/mgc/mbti/config/WebConfig.java
+++ src/main/java/com/mgc/mbti/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("'http://52.78.85.15:8080")
+                .allowedOrigins("http://52.78.85.15:8080")
                 .allowedMethods("GET");
     }
 }
