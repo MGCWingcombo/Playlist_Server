4c03f56f9a995f9fdcc633f76157fb4afb07f846
YongWoo <aba1430@naver.com>
2022-05-31 14:54:53 +0900

    [FIX] 그동안 쓰던 Stream 코드를 제거하고 로직을 간략화함

--- src/main/java/com/mgc/mbti/service/MusicService.java
+++ src/main/java/com/mgc/mbti/service/MusicService.java
@@ -2,6 +2,7 @@ package com.mgc.mbti.service;
 
 import com.mgc.mbti.domain.Music;
 import com.mgc.mbti.domain.Playlist;
+import com.mgc.mbti.dto.MusicDto;
 import com.mgc.mbti.exception.exception.NotFoundTagException;
 import com.mgc.mbti.repository.MusicRepository;
 import com.mgc.mbti.repository.PlaylistRepository;
@@ -21,18 +22,12 @@ public class MusicService {
     private final PlaylistRepository playlistRepository;
     private final MusicRepository musicRepository;
 
-    public List<Map<String,String>> getMusicList(String tag){
+    public List<MusicDto> getMusicList(String tag){
         Playlist playlist = playlistRepository.findByTag(tag);
         if (playlist == null)
             throw new NotFoundTagException();
-        List<Music> musics = musicRepository.findAllByPlayList(playlist);
-        List<Map<String, String>> musicList = musics.stream().map(
-                music -> {
-                    Map<String, String> map = Map.of("Music",music.getMusic(),"Artist",music.getArtist(),"url",music.getUrl(),"urlImg",music.getUrlImg());
-                    return map;
-                }
-        ).collect(Collectors.toList());
+        List<MusicDto> musics = musicRepository.findAllByPlayList(playlist);
 
-        return musicList;
+        return musics;
     }
 }
