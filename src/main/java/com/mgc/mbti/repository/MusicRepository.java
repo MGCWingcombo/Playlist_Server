f913b77f8ed241241adfa2ff6557817ca07e5afe
YongWoo <aba1430@naver.com>
2022-05-31 14:54:34 +0900

    [FIX] 쓸모없어진 import 제거

--- src/main/java/com/mgc/mbti/repository/MusicRepository.java
+++ src/main/java/com/mgc/mbti/repository/MusicRepository.java
@@ -1,15 +1,10 @@
 package com.mgc.mbti.repository;
 
 import com.mgc.mbti.domain.Music;
-import com.mgc.mbti.domain.Playlist;
 import com.mgc.mbti.repository.customRepo.MusicCustomRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
-
+@Repository
 public interface MusicRepository extends JpaRepository<Music, Long> , MusicCustomRepository {
 }
