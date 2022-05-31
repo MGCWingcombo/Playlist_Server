abf0e5afd07b5b288f00b77a663978f6d5eca1b8
YongWoo <aba1430@naver.com>
2022-05-31 14:54:03 +0900

    [ADD] Response용으로 필요한 값만 담는 MusicDto

---
+++ src/main/java/com/mgc/mbti/dto/MusicDto.java
@@ -0,0 +1,15 @@
+package com.mgc.mbti.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MusicDto {
+
+    private String music;
+
+    private String artist;
+
+    private String url;
+
+    private String urlImg;
+}
